349d3d165d554be1d996690bb82ff975ad55c9d1
Jacobel <148277388+elohejacs@users.noreply.github.com>
2025-03-12 09:28:39 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,19 +2,16 @@
 """" Top Ten Limit"""
 import requests
 
-
 def top_ten(subreddit):
-    """"top ten"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
-        .format(subreddit)
-
-    res = requests.get(url,
-                       headers={
-                           'User-Agent': 'Mozilla/5.0'})
-
-    if res.status_code != 200:
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "ALU-Reddit-Query"}
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    
+    if response.status_code != 200:
         print(None)
-    else:
-        json_response = res.json()
-        posts = json_response.get('data').get('children')
-        [print(post.get('data').get('title')) for post in posts]
+        return
+    
+    data = response.json().get("data", {}).get("children", [])
+    for post in data:
+        print(post.get("data", {}).get("title"))
